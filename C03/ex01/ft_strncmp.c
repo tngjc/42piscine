4e8fd9826bf4e8fcb98004c903d3273ab46699b6
tngjc <tng.jeancharles@gmail.com>
2023-07-05 18:16:48 +0200

    added C03

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jtaing <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 12:21:30 by jtaing            #+#    #+#             */
-/*   Updated: 2023/07/04 14:00:50 by jtaing           ###   ########.fr       */
+/*   Updated: 2023/07/05 16:53:18 by jtaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	
 	i = 0;
 	if (n == 0)
-		return ('\0');
+		return (0);
 	while (	s1[i] && s2[i] && (s1[i] == s2[i]) && (i < n - 1))
 		i++;
 	return (s1[i] - s2[i]);
--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,10 +6,9 @@
 /*   By: jtaing <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 10:34:56 by jtaing            #+#    #+#             */
-/*   Updated: 2023/07/05 13:49:30 by jtaing           ###   ########.fr       */
+/*   Updated: 2023/07/05 18:13:07 by jtaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
@@ -17,21 +16,21 @@ char	*ft_strstr(char *str, char *to_find)
 
 	i = 0;
 	j = 0;
-	while (str[i] && str[i] != to_find(0))
+	if (to_find[i] == '\0')
+		return (str);
+	while (str[i])
 	{
-		if (str[i] == to_find[0])
+		while (str[i + j] == to_find[j])
 		{
-			if (str[i + 1] == to_find[j + 1])
-				j++;
-						
-
-				
+			if (to_find[j + 1] == '\0')
+				return (str + i);
+			j++;
 		}
 		i++;
+		j = 0;
 	}
-	return (1);
+	return (0);
 }
-
 #include <string.h>
 #include <stdio.h>
 
@@ -59,4 +58,4 @@ int	main(void)
 	printf("%s\n", strcmp(s3a, s3b) == 0 && strcmp(s4a, s4b) == 0 ? "Success" : "Fail");
 	printf("%s:%s\n", ft_strstr(s5a, s6a), strstr(s5b, s6b));
 	printf("%s\n", strcmp(s5a, s5b) == 0 && strcmp(s6a, s6b) == 0 ? "Success" : "Fail");
-}
+}	
