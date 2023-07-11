8ffe05b481e5484c1d5e3af812277d29f38e1810
tngjc <tng.jeancharles@gmail.com>
2023-07-11 13:57:36 +0200

    added

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtaing <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 12:47:02 by jtaing            #+#    #+#             */
+/*   Updated: 2023/07/11 13:44:47 by jtaing           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	a;
+
+	a = 0;
+	while (str[a])
+	{
+		ft_putchar(str[a]);
+		a++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j < ac - 1)
+	{
+		i = 1;
+		while (i < ac - 1)
+		{
+			if (ft_strcmp(av[i], av[i + 1]) > 0)
+				ft_swap(&av[i], &av[i + 1]);
+			i++;
+		}
+		j++;
+	}
+	i = 1;
+	while (i <ac)
+	{
+		ft_putstr(av[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
+
