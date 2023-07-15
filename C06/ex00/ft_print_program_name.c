1f3a97afa8fb0343c52d66b289ef1abb9646f039
tngjc <tng.jeancharles@gmail.com>
2023-07-15 17:33:28 +0200

    added

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -6,26 +6,23 @@
 /*   By: jtaing <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 09:37:22 by jtaing            #+#    #+#             */
-/*   Updated: 2023/07/13 11:59:29 by jtaing           ###   ########.fr       */
+/*   Updated: 2023/07/15 17:06:51 by jtaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write (1, &c, 1);
-}
 
 int	main(int ac, char *av[])
 {
 	int	i;
 
 	i = 0;
-	while (av[0][i] != '\0' && ac > 0)
+	if (ac < 0)
+		return (0);
+	while (av[0][i] != '\0')
 	{
-		ft_putchar(av[0][i]);
-			i++;
+		write(1, &av[0][i], 1);
+		i++;
 	}
-	ft_putchar('\n');
+	write(1, "\n", 1);
 	return (0);
 }
